e192cd736e61127c1be65d8262b4461b868a7c0b
Jerry <aca.jerryh@gmail.com>
2024-09-25 23:00:17 -0400

    Add spinner and spawner

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
 
+/// <summary>
+/// Spin an object at a constant speed.
+/// </summary>
 public class Spinner : MonoBehaviour
 {
+    
+    [SerializeField]
+    float speed = 5f;   // User-defined speed
 
-
-
+    // Start is called before the first frame update
     void Start()
     {
-        float angle = Random.Range(-45f, 45f);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        /* Make the object start at a random orientation. */
+        float angle = Random.Range(-180f, 180f);
+
+        // Your code here
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, 15f * Time.deltaTime);
+        /* Spin the object at a constant speed. */
+
+        // Your code here
+
     }
 }
